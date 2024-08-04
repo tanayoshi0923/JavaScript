9dd78127eb19dc2bc06bdfdd7dad40cbc6394dde
tanayoshi0923 <affiproger.tanayoshi@gmail.com>
2024-08-04 20:45:00 +0900

    [update]完成

--- MySlideshow/main.js
+++ MySlideshow/main.js
@@ -2,9 +2,58 @@
 
 {
   const thumbnails = document.querySelectorAll('.thumbnail');
-  const mainImage = document.getElementById('main-image');
+  const mainImage = document.querySelector('#main-image');
+  const next =document.querySelector('#next');
+  const prev =document.querySelector('#prev');
+  let activeIndex = 0;
+
+  thumbnails[0].addEventListener('click', () => {
+    activeIndex = 0;
+    mainImage.src = thumbnails[0].src;
+    thumbnails[0].classList.add('active');
+    thumbnails[1].classList.remove('active');
+    thumbnails[2].classList.remove('active');
+  });
 
   thumbnails[1].addEventListener('click', () => {
+    activeIndex = 1;
     mainImage.src = thumbnails[1].src;
+    thumbnails[0].classList.remove('active');
+    thumbnails[1].classList.add('active');
+    thumbnails[2].classList.remove('active');
   });
+
+  thumbnails[2].addEventListener('click', () => {
+    activeIndex = 2;
+    mainImage.src = thumbnails[2].src;
+    thumbnails[0].classList.remove('active');
+    thumbnails[1].classList.remove('active');
+    thumbnails[2].classList.add('active');
+  });
+
+  next.addEventListener('click', () => {
+    activeIndex++;
+    if(activeIndex > 2) {
+      activeIndex = 0;
+    }
+
+    mainImage.src = thumbnails[activeIndex].src;
+    thumbnails[0].classList.remove('active');
+    thumbnails[1].classList.remove('active');
+    thumbnails[2].classList.remove('active');
+    thumbnails[activeIndex].classList.add('active')
+  })
+
+  prev.addEventListener('click', () => {
+    activeIndex--;
+    if(activeIndex < 0) {
+      activeIndex = 2;
+    }
+
+    mainImage.src = thumbnails[activeIndex].src;
+    thumbnails[0].classList.remove('active');
+    thumbnails[1].classList.remove('active');
+    thumbnails[2].classList.remove('active');
+    thumbnails[activeIndex].classList.add('active')
+  })
 }
